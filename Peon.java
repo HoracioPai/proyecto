0434a97f984d8c1da7197156a262d1cc3f65ddf4
HoracioPai <horaciomares@ciencias.unam.mx>
2022-01-28 19:53:54 -0600

    pieza

---
+++ Peon.java
@@ -0,0 +1,38 @@
+import Tablero;
+public class Rey extends Pieza {
+
+    @Override
+    boolean[][] posicionesPosibles() {
+        boolean [][] posicionesPosibles = new boolean [6][6];
+        int x = this.getPosicion().getX();
+        int y = this.getPosicion().getY();
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 6; j++){
+                if (i == x || i == x+1 || i == x-1){
+                    if(j == y || j == y+1 || j == y-1)
+                        posicionesPosibles[i][j] = true;
+                }                   
+            }
+        }
+        posicionesPosibles[x][y] = false;
+        return posicionesPosibles;
+    }
+    
+    @Override
+     boolean movimientoPosible(Posicion posicionNueva, Tablero tablero){
+           int x1 = getPosicion().getX();
+           int y1 = getPosicion().getY();
+           int x2 = posicionNueva.getX();
+           int y2 = posicionNueva.getY();
+         
+           
+       if(!posicionesPosibles()[x2][y2]){
+           return false;
+       }
+       if (piezasDelMismoEquipo(tablero)[x2][y2]){
+           return false;
+       }
+        return true;
+    }
+    
+}
